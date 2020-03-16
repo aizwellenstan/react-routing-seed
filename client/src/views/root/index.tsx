26cc66dfcb757b76240b17c223b11a556758b744
aizwellenstan <aizwellenstan@gmail.com>
2020-03-16 15:06:50 +0900

    startup

---
+++ client/src/views/root/index.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { ThemeProvider, styled } from 'client/src/styles';
+import { I18nextProvider } from 'react-i18next';
+import { getLocale } from 'client/src/locales';
+import { Switch, Route } from 'react-router-dom';
+import { theme } from 'client/src/styles/theme';
+import { Banner } from 'client/src/components/banner';
+import { GlobalStyle } from './global-style';
+
+// import { Events } from 'client/src/views/events';
+import { Home } from 'client/src/views/home';
+import { Login } from 'client/src/views/login';
+
+const locale = getLocale();
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: ${({ theme }) => theme.backgroundNormal};
+`;
+
+export const Root: React.SFC = React.memo(() => {
+  return (
+    <>
+      <I18nextProvider i18n={locale}>
+        <ThemeProvider theme={theme}>
+          <Wrapper>
+            <Banner />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+            </Switch>
+            <GlobalStyle />
+          </Wrapper>
+        </ThemeProvider>
+      </I18nextProvider>
+    </>
+  );
+});
---
+++ client/src/views/settings/index.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+// import { TimetableContainer } from 'client/src/containers/timetable-container';
+import { SidebarContainer } from 'client/src/containers/sidebar-container';
+import { Page } from 'client/src/components/page';
+
+import { styled } from 'client/src/styles';
+import { sidebarWidth } from 'client/src/styles/constants';
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  margin-left: 0;
+  overflow-x: scroll;
+  overflow-y: hidden; /* fixme */
+  -webkit-overflow-scrolling: touch;
+
+  @media screen and (min-width: 700px) {
+    width: calc(100% - ${sidebarWidth}px);
+    margin-left: ${sidebarWidth}px;
+  }
+`;
+
+export const Settings = React.memo(() => {
+  if (localStorage.getItem('login') !== 'true') {
+    localStorage.setItem('pageNow', 'Login');
+    window.location.reload();
+  }
+  return (
+    <Page>
+      <SidebarContainer />
+      <Wrapper>Comming Soon</Wrapper>
+    </Page>
+  );
+});
